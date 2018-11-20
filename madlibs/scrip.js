500a7aa0ddde300154453ab6e484b434fc0b6bbd
Bhatt1028 <43218176+Bhatt1028@users.noreply.github.com>
2018-11-20 14:41:11 -0600

    rew4
    t

---
+++ madlibs/scrip.js
@@ -0,0 +1,20 @@
+
+
+function reveals()
+{
+    var words = [];
+    words.push(document.getElementById("1").value);
+    words.push(document.getElementById("2").value);
+    words.push(document.getElementById("3").value);
+    words.push(document.getElementById("4").value);
+    words.push(document.getElementById("5").value);
+    words.push(document.getElementById("6").value);
+    words.push(document.getElementById("7").value);
+    words.push(document.getElementById("8").value);
+    words.push(document.getElementById("9").value);
+    words.push(document.getElementById("10").value);
+    alert(words);
+    return "trsgesdfvrs" + words[0] + "asdfr" + words[1] + "asdfr" + words[2] + "asdfr" + words[3] + "asdfr" + words[4] + "asdfr"
+        + words[5] + "asdfr" + words[6] + "asdfr" + words[7] + "asdfr" + words[8] + "asdfr" + words[9] + "asdfr";
+}
+    
